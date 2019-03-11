3a7deb4794aaac99ed0f071bfffcf603baac60f4
realliu1996 <realliu1996@163.com>
2019-03-11 16:07:43 +0800

    bootstrap模态框居中

--- web/dist/js/bootstrap.js
+++ web/dist/js/bootstrap.js
@@ -992,6 +992,17 @@ if (typeof jQuery === 'undefined') {
         that.$element[0].offsetWidth // force reflow
       }
 
+        that.$element.children().eq(0).css("position", "absolute").css({
+            "margin": "0px",
+            "top": function () {
+                return (that.$element.height() - that.$element.children().eq(0).height() - 40) / 2 + "px";
+            },
+            "left": function () {
+                return (that.$element.width() - that.$element.children().eq(0).width()) / 2 + "px";
+            }
+        });
+
+
       that.$element
         .addClass('in')
         .attr('aria-hidden', false)
